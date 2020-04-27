d9482c52c398cd4a5ae186e2d9df832ea83044bb
nononi <13261065332@163.com>
2020-04-27 22:22:23 +0800

    cookie util

--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -3,6 +3,7 @@ package com.mmall.service.impl;
 import com.google.common.collect.Lists;
 import com.mmall.service.IFileService;
 import com.mmall.util.FTPUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -16,16 +17,16 @@ import java.util.UUID;
  * Created by NoNo_Ni on 2020/4/8.
  */
 @Service("iFileService")
+@Slf4j
 public class FileServiceImpl implements IFileService {
 
-    private Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
 
     public String upload(MultipartFile file,String path){
         String fileName = file.getOriginalFilename();
         //扩展名
         String fileExtensionName = fileName.substring(fileName.lastIndexOf('.')+1);
         String uploadFileName = UUID.randomUUID().toString()+"."+fileExtensionName;
-        logger.info("开始上传文件，上传文件的文件名:{},上传的路径:{},新文件名:{}",fileName,path,uploadFileName);
+        log.info("开始上传文件，上传文件的文件名:{},上传的路径:{},新文件名:{}",fileName,path,uploadFileName);
 
         File fileDir = new File(path);
         if(!fileDir.exists()){
@@ -44,7 +45,7 @@ public class FileServiceImpl implements IFileService {
             targetFile.delete();
 
         } catch (IOException e) {
-            logger.error("上传文件异常",e);
+            log.error("上传文件异常",e);
         }
 
         return targetFile.getName();
---
+++ src/main/java/com/mmall/util/JsonUtil.java
@@ -0,0 +1,151 @@
+package com.mmall.util;
+
+import com.google.common.collect.Lists;
+import com.mmall.pojo.User;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+import org.codehaus.jackson.type.JavaType;
+import org.codehaus.jackson.type.TypeReference;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by NoNo_Ni on 2020/4/24.
+ */
+@Slf4j
+public class JsonUtil {
+
+    private static ObjectMapper objectMapper=new ObjectMapper();
+    static{
+        //对象的所有字段全部列入
+        objectMapper.setSerializationInclusion(Inclusion.ALWAYS);
+
+        //取消默认转换timestamps
+        objectMapper.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS,false);
+
+        //忽略空bean转json的错误
+        objectMapper.configure(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS,false);
+
+        //所有的日期格式都统一为以下的样式，即yyyy-MM-dd HH:mm:ss
+        objectMapper.setDateFormat(new SimpleDateFormat(DateTimeUtil.STANDARD_FORMAT));
+
+        //忽略在json字符串中存在,但是在java对象中不存在属性的情况，防止错误
+        objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+
+    }
+
+    public static <T> String obj2String(T obj){
+        if(obj==null){
+            return null;
+        }
+        try {
+            return obj instanceof String ? (String)obj : objectMapper.writeValueAsString(obj);
+        } catch (IOException e) {
+            log.warn("Parse object to String error",e);
+            return null;
+        }
+    }
+
+    //返回格式化地json
+    public static <T> String obj2StringPretty(T obj){
+        if(obj==null){
+            return null;
+        }
+        try {
+            return obj instanceof String ? (String)obj : objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        } catch (IOException e) {
+            log.warn("Parse Object to String error",e);
+            return null;
+        }
+    }
+
+    public static <T> T string2Obj(String str,Class<T> clazz){
+        if(StringUtils.isEmpty(str) || clazz==null){
+            return null;
+        }
+
+        try {
+            return clazz.equals(String.class) ? (T)str : objectMapper.readValue(str,clazz);
+        } catch (IOException e) {
+            log.warn("Parse String to Object error",e);
+            return null;
+        }
+
+    }
+
+    public static <T> T string2Obj(String str, TypeReference<T> typeReference){
+        if(StringUtils.isEmpty(str) || typeReference==null){
+            return null;
+        }
+
+        try {
+            return (T) (typeReference.getType().equals(String.class)? str : objectMapper.readValue(str,typeReference));
+        } catch (IOException e) {
+            log.warn("Parse String to Object error",e);
+            return null;
+        }
+    }
+
+
+    public static <T> T string2Obj(String str, Class<?> collectionClass,Class<?>... elementClasses){
+
+        JavaType javaType = objectMapper.getTypeFactory().constructParametricType(collectionClass,elementClasses);
+        if(StringUtils.isEmpty(str)){
+            return null;
+        }
+
+        try {
+            return objectMapper.readValue(str,javaType);
+        } catch (IOException e) {
+            log.warn("Parse String to Object error",e);
+            return null;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        User u1 = new User();
+        u1.setId(1);
+        u1.setEmail("geely@happymmall.com");
+
+        User u2 = new User();
+        u2.setId(2);
+        u2.setEmail("nononi@happymmall.com");
+
+        String user1Json = JsonUtil.obj2String(u1);
+        String user1JsonPretty = JsonUtil.obj2StringPretty(u1);
+
+        log.info("user1Json:{}",user1Json);
+
+        log.info("user1JsonPretty:{}",user1JsonPretty);
+        User user = JsonUtil.string2Obj(user1JsonPretty,User.class);
+
+        List<User> userList = Lists.newArrayList();
+        userList.add(u1);
+        userList.add(u2);
+
+        String userListStr = JsonUtil.obj2StringPretty(userList);
+
+        log.info("==============");
+
+        log.info(userListStr);
+
+
+        List<User> userListObj = JsonUtil.string2Obj(userListStr, new TypeReference<List<User>>() {
+        });
+
+        List<User> userListObj2 = JsonUtil.string2Obj(userListStr,List.class,User.class);
+
+
+        System.out.println("end");
+    }
+
+
+}
